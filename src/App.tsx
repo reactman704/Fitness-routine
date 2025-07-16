573223cc8e72e0ef96805b2f13fa448935fea4aa
Greatteacherjubro <wisdomjh100@gmail.com>
2025-07-17 02:28:24 +0900

    feat: 카테고리 버튼 및 운동 필터 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+
 import './App.css'
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
+  const [exercises, setExercise] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState('');
+
+  const categoryList = [...new Set(exercises.map((exercise)=>(exercise.category_name)))];
+
+  const filterExercise = exercises.filter((exericse)=>(
+    exericse.category_name === selectedCategory
+  ));
+
+  
+
+  useEffect(() => {
+    fetchData();
+  
+    async function fetchData(){
+      const url = 
+      'https://gist.githubusercontent.com/reactman704/8d5aa05caf0bc1cc505ab76356e0fe0a/raw/835937c21f2beb658f10bf2a2c0d8e0dfacffb87/gistfile1.json';
+
+      try {
+        const response = await axios.get(url);
+        
+        
+        setExercise(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    
+  }, [])
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='p-5 w-fill'>
+        <div className='w-full flex flex-wrap gap-3'>
+          {categoryList.map((category)=>(
+            <button
+              key={category}
+              onClick={ () => (setSelectedCategory(category)) }
+              className={`px-4 py-2 rounded-full border ${
+                selectedCategory === category ? 'bg-blue-500 text-white' : 'bg-gray-200'
+              }`}
+            >
+              {category}
+            </button>
+          ))}
+        </div>
+
+        <div className='flex flex-wrap p-5'>
+          { filterExercise.map((exercise)=>(
+            <div key={exercise.id} className='p-10 border w-'>
+              <div>{exercise.exercise_name}</div>
+              <div>{exercise.description}</div>
+            </div>
+          )) }
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
